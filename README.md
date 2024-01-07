d5ed9ea89f5fef781f243059bc3349b24d3ff5bd
Gaurav <gauravy1514@gmail.com>
2024-01-07 18:09:34 +0530

    Form Validation

--- README.md
+++ README.md
@@ -3,9 +3,11 @@
 - Create React App
 - Configured TailwindCSS
 - Header
+- Routing of App
 - Login Form
 - Sign Up Form
-- 
+- Form Validation
+- useRef Hook
 
 # FEATURES
 * LogIn/SignUp Page
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,27 @@
 // import React from 'react'
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Header from './Header'
+import { checkValidData } from '../utils/validate';
 
 const Login = () => {
+  const email = useRef(null);
+  const password = useRef(null);
+  const name = useRef(null);
+
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
+  };
+  
+  const handleButtonClick = () => {
+    //Firstly Validate the Form data
+    // console.log(email.current.value);
+
+    const message = checkValidData(email.current.value, password.current.value, name.current.value);
+    setErrorMessage(message);
+    //If Email and Password are correct then proceede to SignIn/SignUp
   };
 
   return (
@@ -18,26 +33,33 @@ const Login = () => {
                 alt='logo' 
             />
         </div>
-        <form className='absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
+        <form onSubmit={(e) => e.preventDefault()} className='absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
           <h1 className=' font-bold text-3xl py-4'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
           
           {!isSignInForm && <input 
+            ref = {name}
             type="text" 
             placeholder="Full Name" 
             className='p-4 my-4 w-full bg-gray-800 rounded-lg'
           />}
           <input 
+            ref = {email}
             type="text" 
             placeholder="Enter Email or Number" 
             className='p-4 my-4 w-full bg-gray-800 rounded-lg'>
           </input>
           <input 
+            ref = {password}
             type="password" 
             placeholder="Password" 
             className='p-4 my-4 w-full bg-gray-800 rounded-lg'>
           </input>
+          <p className='text-red-500 font-bold py-2'>{errorMessage}</p>
           
-          <button className='p-4 my-6 bg-red-700 w-full rounded-lg'>{isSignInForm ? "Sign In" : "Sign Up"}</button>
+          <button className='p-4 my-6 bg-red-700 w-full rounded-lg +' onClick={handleButtonClick}>
+            {isSignInForm ? "Sign In" : "Sign Up"}
+          </button>
+
           <p className='py-4 cursor-pointer' onClick={toggleSignInForm}>{isSignInForm ? "New to Netflix? Sign Up Now" : "Already Registered? Sign In Now"}</p>
         </form>
     </div>
